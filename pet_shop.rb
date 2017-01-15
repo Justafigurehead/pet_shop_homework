d839ad49bf0cf9b275d18ff6605aeea82e395644
Justafigurehead <strachanmichaela@hotmail.com>
2017-01-15 18:50:09 +0000

    Homework updated

--- pet_shop.rb
+++ pet_shop.rb
@@ -16,7 +16,7 @@ def add_or_remove_cash(pet_shop, cash)
   pet_shop[:admin][:total_cash] += cash
   # total_cash += cash
   # return total_cash
-end_
+end
 
 def pets_sold(sold)
    return sold[:admin][:pets_sold]
@@ -41,12 +41,52 @@ def pets_by_breed(pet_shop, breed)
 end
 
 def find_pet_by_name(pet_shop, pet_name)
-  find_pet_by_name = []
   for pet in pet_shop[:pets]
     if pet[:name] == pet_name
-      find_pet_by_name.push( pet_name )
+      return pet 
     end
   end
-  return find_pets_by_name
+     return nil
 end
 
+def remove_pet_by_name(pet_shop, pet_name)
+  pet = find_pet_by_name(pet_shop, pet_name)
+  pet_shop[:pets].delete( pet )
+end
+
+
+def add_pet_to_stock(pet_shop, new_pet)
+pet_shop[:pets] << new_pet
+end
+
+
+def customer_pet_count(customer)
+  customer[:pets].count
+end
+
+# def add_or_remove_cash(pet_shop, cash)
+#   pet_shop[:admin][:total_cash] += cash
+
+def add_pet_to_customer(customer, new_pet)
+  
+  customer[:pets] << new_pet
+
+end
+
+def customer_can_afford_pet(customer, new_pet)
+   if (customer[:cash] >= new_pet[:price])
+     return true
+   else  
+    return false
+  end 
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  customer_sale = customer_can_afford_pet(customer, pet)
+  
+ 
+  if (customer_sale == true)
+    customer_buys_pet = add_pet_to_customer(customer, pet)
+   return customer_buys_pet
+ end
+end
